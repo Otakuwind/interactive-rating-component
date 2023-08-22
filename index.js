3017673cd1c0b0931f4917221e91bdd6ea07247e
Edgar Nwajei <edgarlinked@gmail.com>
2023-08-22 10:28:33 +0100

    added an active state

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');        
+    });
+});
